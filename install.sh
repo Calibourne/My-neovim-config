d77a48a2c29793f2dfc6de6898efa7fc61f4cae3
Calibourne <Eddiekanevsky@gmail.com>
2022-09-26 22:43:30 +0300

    install.sh updated

--- install.sh
+++ install.sh
@@ -1,1 +1,24 @@
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+#!/bin/bash
+
+NVIM=/usr/local/bin/nvim
+CONFIG="$HOME"/.config/nvim
+
+if  [ ! -f "$NVIM" ]; then
+    echo "Getting Latest Version of Neovim..." &&
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage &&
+    chmod u+x ./nvim.appimage &&
+    mv ./nvim.appimage "$NVIM" &&
+    echo "Latest Neovim installed"
+fi
+# [[ -f "$CONFIG" ]] || [
+#     echo "Making a backup folder for old configs at $CONVIG_backup"
+#     mv "$CONFIG" ~/.config/nvim_backup
+#     echo "Done"
+# ]
+
+# echo "$CONFIG"
+# if [ ! -d "$Config" ]; then
+#     echo "Copying config files"
+# else
+#     echo "Directory already exists"
+# fi
