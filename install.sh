5d1a80be0d3e0c6c93058c9017e37e0f2a3d70f2
Calibourne <Eddiekanevsky@gmail.com>
2022-09-27 17:19:39 +0300

    install.sh updated

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 NVIM=/usr/local/bin/nvim
 CONFIG="$HOME"/.config/nvim
 
+if [ $EUID != 0 ]; then
+    echo "Can't install neovim. Please try again as root"
+    exit
+fi
+
 if  [ ! -f "$NVIM" ]; then
     echo "Getting Latest Version of Neovim..." &&
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage &&
@@ -10,6 +15,12 @@ if  [ ! -f "$NVIM" ]; then
     mv ./nvim.appimage "$NVIM" &&
     echo "Latest Neovim installed"
 fi
+
+echo "Getting 'Packer' plugin manager..."
+curl -LO https://raw.github.com/robertpeteuil/packer-installer/master/packer-install.sh
+chmod +x ./packer-install.sh
+# ./packer-install.sh
+
 # [[ -f "$CONFIG" ]] || [
 #     echo "Making a backup folder for old configs at $CONVIG_backup"
 #     mv "$CONFIG" ~/.config/nvim_backup
